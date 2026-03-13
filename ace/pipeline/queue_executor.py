191cec10c8a803aacc3c4a1d33913b5824ebb4b0
alysonloureiro2011-star <alysonloureiro2011@gmail.com>
2026-03-13 13:55:07 -0300

    Update queue_executor.py

--- ace/pipeline/queue_executor.py
+++ ace/pipeline/queue_executor.py
@@ -1,1 +1,35 @@
 
+import queue
+import threading
+import time
+
+
+class QueueExecutor:
+
+    def __init__(self):
+        self.q = queue.Queue()
+        self.running = False
+
+    def add(self, task):
+        self.q.put(task)
+
+    def worker(self):
+        while self.running:
+            try:
+                task = self.q.get(timeout=2)
+                task()
+                self.q.task_done()
+            except queue.Empty:
+                time.sleep(1)
+
+    def start(self):
+        if self.running:
+            return
+
+        self.running = True
+
+        t = threading.Thread(target=self.worker, daemon=True)
+        t.start()
+
+    def stop(self):
+        self.running = False
