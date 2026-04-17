ba4cb8021d7646fe0bd8483a58ec9e422038536a
alysonloureiro2011-star <alysonloureiro2011@gmail.com>
2026-04-17 15:05:55 -0300

    Initialize platform_adapters with YouTube, TikTok, and Threads

---
+++ ace_next/platform_adapters/__init__.py
@@ -0,0 +1,9 @@
+from .youtube import YouTubeAdapter
+from .tiktok import TikTokAdapter
+from .threads import ThreadsAdapter
+
+__all__ = [
+    "YouTubeAdapter",
+    "TikTokAdapter",
+    "ThreadsAdapter",
+]
